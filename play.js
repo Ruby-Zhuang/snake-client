66889b86f0e4c4f6c614b29e568511a7af209717
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-04-06 22:12:54 -0400

    Setup Stdin

--- play.js
+++ play.js
@@ -1,3 +1,29 @@
 const { connect } = require('./client');
+/**
+ * Setup User Interface
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  // "data" callback handler for stdin
+  const handleUserInput = (data) => {
+    // Check for ctrl + c input to terminate the game
+    if (data === '\u0003') {
+      process.exit();
+    }
+  };
+
+  // On any input from stdin
+  process.stdin.on('data', handleUserInput);
+  
+  return stdin;
+};
+
+
 console.log('Connecting ...');
 connect();
+setupInput();
