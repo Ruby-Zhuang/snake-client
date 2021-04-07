105671304fc41f405735f19165c0407bf4a061e3
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-04-07 14:29:08 -0400

    Testing with setInterval

--- input.js
+++ input.js
@@ -1,4 +1,4 @@
-// Require
+// Require constants
 const { MOVES, MESSAGES } = require('./constants');
 
 // Stores the active TCP connection object.
@@ -16,24 +16,38 @@ const setupInput = function(conn) {
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
+  // "data" callback handler for stdin
   process.stdin.on('data', handleUserInput); // On any input from stdin
   return stdin;
 };
 
-// "data" callback handler for stdin
-const handleUserInput = (key) => {
-  // Check for ctrl + c input to terminate the game
-  if (key === '\u0003') process.exit();
+// Keep track of the current move
+let currentMove;
 
+// Handle user input
+const handleUserInput = (data) => {
+  // Function to make the next snake move continuously using setInterval
+  const nextMove = (key) => {
+    currentMove = setInterval(() => connection.write(`Move: ${key}`), 100);
+  };
+  // Check for ctrl + c input to terminate the game
+  if (data === '\u0003') process.exit();
+  
   // Send Movement Keys (WASD) to server
-  if (MOVES[key]) connection.write(`Move: ${MOVES[key]}`);
+  if (MOVES[data]) {
+    {
+      clearInterval(currentMove);
+      nextMove(MOVES[data]);
+    }
+  }
+  //connection.write(`Move: ${MOVES[key]}`);
   // if (key === 'w') connection.write('Move: up');
   // if (key === 'a') connection.write('Move: left');
   // if (key === 's') connection.write('Move: down');
   // if (key === 'd') connection.write('Move: right');
-
+  
   // Send messages to the server
-  if (MESSAGES[key]) connection.write(`Say: ${MESSAGES[key]}`);
+  if (MESSAGES[data]) connection.write(`Say: ${MESSAGES[data]}`);
   // if (key === 'h') connection.write('Say: HI! :)');
   // if (key === 'm') connection.write('Say: meow~');
   // if (key === 'z') connection.write('Say: ZzZzZzzzzz');
