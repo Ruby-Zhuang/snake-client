61c35b1c82fe92d05046883a6a45cb97b28a769e
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-04-06 21:29:14 -0400

    Send snake initials to server

--- client.js
+++ client.js
@@ -11,6 +11,13 @@ const connect = function() {
 
   // interpret incoming data as text
   conn.setEncoding('utf8');
+
+  // Print message to screen when connection is successfully established
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server!');
+    conn.write('Name: RZ!');
+  });
+
   // Message from server (event handler to handle incoming data)
   conn.on('data', (data) => {
     console.log(data);
